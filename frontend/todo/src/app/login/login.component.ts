2d76c4c7ea9ced29137670ba8aafa1d3f00359d4
alineavila <aline.avila01@gmail.com>
2020-02-26 00:08:21 -0300

    aprendendo sobre o ngModel

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'aline'
+  password = ''
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleLogin(){
+    console.log(this.username);
+  }
 }
