9df1481b4291eda122361436a2c5df5c57cc8e27
alineavila <aline.avila01@gmail.com>
2020-03-15 16:20:44 -0300

    criação de classe boas práticas

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor (
+    public id: numer,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,12 +19,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [
-    {id: 1, description: 'Learn to Dance'},
-    {id: 2, description: 'Learn to Dance'},
-    {id: 3, description: 'Learn to Dance'}
-  ]
+  // todos = [
+  //   {id: 1, description: 'Learn to Dance'},
+  //   {id: 2, description: 'Learn to Dance'},
+  //   {id: 3, description: 'Learn to Dance'}
+  // ] 
+  // // a melhor prática é ter uma classe para ter todos esses
 
+  todos = [
+  new Todo(1, 'Learn', false, new Date()),
+  new Todo(2, 'Learn', false, new Date()),
+  new Todo(2, 'Learn', false, new Date()),
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
